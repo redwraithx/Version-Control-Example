5e46b6404f8a0129804015694dc9b31798f67221
RedWraith <brianpurdy01@gmail.com>
2020-01-05 21:26:54 -0400

    Player Character and controller script added.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed = 8f;
+    [SerializeField] private float _sprintSpeed = 5f;
+    [SerializeField] private float _currentSpeed = 0f;
+    private Vector3 _moveDirection;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        _moveDirection = new Vector3(moveHorizontal, moveVertical, 0f);
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _currentSpeed = _movementSpeed + _sprintSpeed;
+        }
+        else
+        {
+            _currentSpeed = _movementSpeed;
+        }
+
+        transform.Translate(_moveDirection * _currentSpeed * Time.deltaTime);
+    }
+}
